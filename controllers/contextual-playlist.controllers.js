421481e7e2cd69b5a6ce935adc2f11362f4ea9eb
Marcos Baratta <129697318+marcosbaratta@users.noreply.github.com>
2023-08-17 10:15:17 -0300

    creacion playlist

--- controllers/contextual-playlist.controllers.js
+++ controllers/contextual-playlist.controllers.js
@@ -32,12 +32,59 @@ exports.getFilteredSongs = async (req, res)=>{
       // generar playlist y agarrar su id
       const filter = await knex.select("songs.name as song_name", 'artists.name as artist_name', 'songs.duration as song_duration', 'songs.id' ).from("songs").innerJoin('songs_moods', 'songs.id','songs_moods.songs_id').innerJoin('songs_weathers', 'songs.id','songs_weathers.songs_id').innerJoin('songs_occasion', 'songs.id','songs_occasion.songs_id').innerJoin('artists', 'songs.artist_id', 'artists.id').where(req.body).whereIn('genre_id', genre_id).distinctOn('songs.id');
       console.log(filter);
- 
+
+      const userEmail = req.headers.username;
+      console.log(userEmail);
+      const userId = await knex.select('users.id').from('users').where('users.email',userEmail);
+      console.log(userId);
+
+      const newPlaylist = { 
+        user_id: userId[0].id,
+        name: userId[0].id.toString()
+      };
+      
+
+      const playlistId = await knex('playlists').insert(newPlaylist).returning('id');
+      console.log('playlist id es: ',playlistId);
+
+
+      // const insertToPlaylist_songs = await 
+
+
+      filter.forEach( async (element) => {
+        const playlist_songs = {
+          playlist_id: playlistId[0].id,
+          song_id: element.id
+        }
+        // console.log(playlist_songs);
+        const insertToPlaylist_songs = await knex('playlist_songs').insert(playlist_songs)
+        
+      });
+
+
+      
+      // exports.crearNuevaTarea = async (req, res) => {
+      //   const tareas = await knex('tareas').insert(req.body).returning ("*");
+      //   console.log("nueva tarea");
+      //   res.json({ resultado: tareas });
+      // };
+      
+
+      
+
+
+      
+// nacho 
 //insertar en playlist_song con el playlist_id todos los song_id de las canciones de la variable filter
 //devolver el playlist_id para luego pedir esta playlist desde el front en otra ruta
 //pasar el playlist_id a traves de react router de un componente a otro usando route-params
 //crear el endpoint para obtener playlist y sus canciones por id
-      
+
+// fran
+// - generar una playlist, hacer un insert en la tabla playlists y luego obtener el ID generado
+// - para cada canción del filter (filtrado) recorrer e insertar en la tabla playlist_songs utilizando el id de la canción (que obtenemos en el recorrido) y el id del punto 1
+
+// si todo va bien, realizar un select * playlist join playlistsongs join canciones utilizando el id del punto 1 y dicho resultado devolver al front
 
 
 
